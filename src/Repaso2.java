22d2fd8ce5d3a9f3e651674e71b0cedc7f0f100d
Jorge Cisneros <fraxito@gmail.com>
2015-11-12 18:04:51 +0100

    Terminado el Ejercicio de Repaso 2

---
+++ src/Repaso2.java
@@ -0,0 +1,43 @@
+/*
+ * Autor Jorge Cisneros:
+ * La clase Repaso 2 tiene que dibujar un rectángulo
+ * a partir del centro de la pantalla, en el lado superior
+ * de la pantalla
+ * 
+ */
+import acm.graphics.*;
+
+public class Repaso2 extends acm.program.GraphicsProgram{
+
+	//declaro una variable de instancia para almacenar el rectángulo
+	//como es de instancia, se ve desde el init y desde el run
+	GRect rectangulo;
+	
+	//declaro otra variable de instancia que guardará el punto X de la
+	//pantalla en que empezará a pintarse el rectángulo
+	int distanciaX;
+	
+	public void init(){
+		//cambio el tamaño de la pantalla
+		setSize(800, 600);
+		
+		//creo el rectángulo de 120 de ancho por 80 de alto
+		rectangulo = new GRect(120,80);
+		add(rectangulo);
+	}
+	
+	public void run(){
+		//divido el ancho de la pantalla en 2 para saber donde está
+		//la mitad exacta de la pantalla
+		distanciaX = getWidth()/2;
+		rectangulo.setLocation(distanciaX, 0);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
